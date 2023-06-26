4b589f7918ac0a7947e9b84f50263d629cd28b66
Xou <137453769+xounen@users.noreply.github.com>
2023-06-26 13:15:24 -0300

    Python exercices

---
+++ Base Code for Years.py
@@ -0,0 +1,8 @@
+
+name = input("digite seu nome:")
+idn = int(input('digite sua idade:'))
+an = 2023
+nasc = an  - idn
+
+print ('parabéns pelo registro senhor(a)' {name} 'você foi registrado com' {idn} 'nascido em' ,nasc)
+
